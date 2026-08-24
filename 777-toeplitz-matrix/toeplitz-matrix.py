9c66b67b3c691b028f0b015646671288284fed3d
Joshua Estime <josh.n.estime@gmail.com>
2026-08-24 01:26:58 -0400

    Time: 2 ms (33.52%) | Memory: 19.2 MB (66.10%) - LeetSync

---
+++ 777-toeplitz-matrix/toeplitz-matrix.py
@@ -0,0 +1,40 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+
+        
+        #define a function to iterate through everyting
+
+        def isdiag(row,col):
+            value=matrix[row][col]
+
+            while(row<len(matrix) and col<len(matrix[0])):
+                if matrix[row][col]!=value:
+                    return False
+
+                row+=1
+                col+=1
+                
+
+            return True
+
+        for col in range(len(matrix[0])):
+                if not isdiag(0,col):
+                    return False
+
+        for row in range(1, len(matrix)):
+                if not isdiag(row,0):
+                    return False
+        return True
+        
+           
+                
+
+            
+
+
+            
+
+       
+            
+                
+        
